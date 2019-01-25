866758014b6887d66a2c2a916903a6e3ebd4a343
Kajetan Janiak <kajetan.janiak@gmail.com>
2019-01-25 12:11:44 +0100

    enumerate, itemize, unicode

--- skrypt.py
+++ skrypt.py
@@ -5,20 +5,31 @@ tex_filename = "main.tex"
 csv_filename = "anki.csv"
 section_split = r"\\section"
 card_split = r"\\subsection"
-double_dolars = "\$\$(.*?)\$\$"
-single_dolars = "\$([^\[\$\]][^\$]*?[^\/\[\$])\$"
+double_dollars = "\$\$(.*?)\$\$"
+single_dollars = "\$([^\[\$\]][^\$]*?[^\/\[\$])\$"
+not_last_item = r"\\item(.*?)\\item"
+last_item_enumerate = r"\\item(.*?)\\end{enumerate}"
+last_item_itemize = r"\\item(.*?)\\end{itemize}"
 
 
 def make_anki(string):
-    double_dolars_str = re.sub(double_dolars, r"[$$]\1[/$$]", string)
-    single_dolars_str = re.sub(single_dolars, r"[$]\1[/$]", double_dolars_str)
-    return single_dolars_str
+    string = re.sub(double_dollars, r"[$$]\1[/$$]", string)
+    string = re.sub(single_dollars, r"[$]\1[/$]", string)
+    # TODO: enumerate & itemize
+    # Założenie: Jedyne zawieranie to itemize wewnątrz enumerate.
+    string = string.replace("\\begin{enumerate}", "<ol>")
+    string = string.replace("\\begin{itemize}", "<ul>")
+    string = re.sub(not_last_item, r"<li>\1</li>\n<li>", string)
+    string = re.sub(last_item_enumerate, r"<li>\1</li>\n</ol>", string)
+    string = re.sub(last_item_itemize, r"<li>\1</li>\n</ul>", string)
+    return string
 
 
 def prepare_card(raw_card, section_title=""):
     raw_front, raw_back = raw_card.split("\n", 1)
     front = raw_front[1:-1]  # Usuwamy { i } z tytułu karty
     raw_back = raw_back.strip("\t")  # TODO: Co to?
+    raw_back = re.sub('\r?\n', '<br>', raw_back)
     # Usuwanie nowej linii z końca?
     back = re.sub(r"[\n]*$", "", raw_back.strip(" ")).rstrip("\n")
     back = back.replace("\\\n", "<br>")
@@ -28,7 +39,7 @@ def prepare_card(raw_card, section_title=""):
 
 
 def main():
-    with open(tex_filename, "r") as file:
+    with open(tex_filename, "r", encoding="utf-8") as file:
         text = file.read()
 
     # Usuwamy \section i dzielimy
@@ -37,9 +48,8 @@ def main():
     anki_cards = []
 
     for section in sections:
-        # Z każdego rozdziału usuwamy \card i dzielimy,
-        # zapominamy o tym co było przed pierwszym \card.
-        # W szczególności zapominamy o tytule rozdziału.
+        # Z każdego rozdziału usuwamy \card i dzielimy.
+        # Wyciągamy tytuł i zapominamy o tym co było przed pierwszym \card.
         section_title, section_text = section.split("\n", 1)
         section_title = section_title[1:-1]  # Usuwamy { i } z tytułu rozdziału
         section_cards = re.split(card_split, section_text)[1:]
@@ -49,5 +59,5 @@ def main():
     df.to_csv(csv_filename, header=None, index=None)
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
